99012183551cd01992a39f643f8416fc61e7f9c5
becc-mu <becc.mulugeta@gmail.com>
2019-01-23 17:45:03 +0000

    update values and X and O can take turns

--- src/index.js
+++ src/index.js
@@ -22,8 +22,11 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    squares[i] = this.state.IsNext ? 'X': 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
@@ -33,7 +36,8 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: X' +
+    (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
